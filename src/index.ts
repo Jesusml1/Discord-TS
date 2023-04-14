25a585e2381954793dc390b96c237b2ebf02bae0
Jose Andres Salazar <joseandres61@gmail.com>
2023-04-14 08:12:02 -0400

    feat: create post functio to save discord post

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,28 @@
-import { Client, GatewayIntentBits, Partials } from "discord.js";
-import path from "node:path";
-import fs from "node:fs";
-import dotenv from "dotenv";
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
+import path from 'path';
+import fs from 'fs';
+import dotenv from 'dotenv';
 dotenv.config();
 
-import { DISCORD_TOKEN } from "./config";
+import { DISCORD_TOKEN } from './config';
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMessages,
-	],
-	partials: [Partials.Channel],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
+  partials: [Partials.Channel],
 });
 
 // Reading event files
-const eventsPath = path.join(__dirname, "events");
-const eventFiles = fs
-	.readdirSync(eventsPath)
-	.filter((file) => file.endsWith(".js") || file.endsWith(".ts"));
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js') || file.endsWith('.ts'));
 
 for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
-client
-	.login(DISCORD_TOKEN)
-	.catch((error) => console.error("Discord.Client.Login.Error", error));
+client.login(DISCORD_TOKEN).catch(error => console.error('Discord.Client.Login.Error', error));
